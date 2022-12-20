aa1eb7119ad4fc7ca51dc6b8cf2344a97c94558c
bodiddlie <bodiddlie@gmail.com>
2022-12-20 15:24:29 -0700

    finish day 20

--- day20/day20.ts
+++ day20/day20.ts
@@ -2,26 +2,39 @@ import { getInput } from "../util";
 
 const sampleInput = ["1", "2", "-3", "3", "-2", "0", "4"];
 
-function partOne(input: string[], times: number = 1) {
-  const numbers = input.map((n) => parseInt(n, 10));
+type Num = {
+  num: number;
+};
 
-  let count = 0;
+function mix(numbers: Num[], times = 1): number {
   const buffer = [...numbers];
 
-  while (count < times) {
-    count++;
-    for (const [idx, num] of numbers.entries()) {
+  while (times--) {
+    for (const n of numbers) {
+      const idx = buffer.indexOf(n);
       buffer.splice(idx, 1);
-      buffer.splice((idx + num) % buffer.length, 0, num);
+      buffer.splice((idx + n.num) % buffer.length, 0, n);
     }
   }
 
-  const zeroIndex = buffer.findIndex((n) => n === 0);
+  const zeroIndex = buffer.findIndex((n) => n.num === 0);
   const landmarks = [1000, 2000, 3000].map(
-    (l) => buffer[(zeroIndex + l) % buffer.length]
+    (l) => buffer[(zeroIndex + l) % buffer.length].num
   );
   return landmarks.reduce((acc, cur) => acc + cur, 0);
 }
 
-// console.log(partOne(sampleInput));
-getInput(20).then(partOne).then(console.log);
+function partOne(input: string[]) {
+  const numbers = input.map((n) => ({ num: Number(n) }));
+  return mix(numbers);
+}
+
+function partTwo(input: string[]) {
+  const numbers = input.map((n) => ({ num: Number(n) * 811589153 }));
+  return mix(numbers, 10);
+}
+
+// partOne(sampleInput);
+// getInput(20).then(partOne).then(console.log);
+// console.log(partTwo(sampleInput));
+getInput(20).then(partTwo).then(console.log);
